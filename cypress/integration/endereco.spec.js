19c1d87c1a76a0a0ff00d13a8cd6f48d27df8f80
name <anderson.netster2012@gmail.com>
2022-04-18 11:56:41 -0300

    arquivo massa de dados

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -3,6 +3,8 @@
 //impprtando arquivo da pasta page-objects arquivo endereco-page.js
 import EnderecoPage from '../support/page-objects/endereco.page'
 
+const dadosEndereco = require('../fixtures/endereco.json')
+
 
     describe('Funcionalidade endereços - Entrega', () => {
     
@@ -26,8 +28,30 @@ import EnderecoPage from '../support/page-objects/endereco.page'
         });
 
         //teste que puxa do arquivo da pasta support/page-object/endereco.page.js
-        it.only('deve fazer cadastro de Faturamento com sucesso', () => {
+        it('deve fazer cadastro de Faturamento com sucesso', () => {
             EnderecoPage.editarEnderecoFaturamento('silva','santos','alliance','Brasil','ione do bem','318','serrana','São Paulo','14150000','11965650796','silva@hotmail.com')
+            cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
+            
+        });
+
+        //teste que puxa do arquivo da pasta fixtures/arquivo.jason. configuração de arquivo na linha 6
+    
+
+        it.only('deve fazer cadastro de Faturamento com sucesso usando arquivo de dados', () => {
+            EnderecoPage.editarEnderecoFaturamento(
+                dadosEndereco[2].nome,
+                dadosEndereco[2].sobrenome,
+                dadosEndereco[2].empresa,
+                dadosEndereco[2].pais,
+                dadosEndereco[2].endereco,
+                dadosEndereco[2].numero,
+                dadosEndereco[2].cidade,
+                dadosEndereco[2].estado,
+                dadosEndereco[2].cep,
+                dadosEndereco[2].telefone,
+                dadosEndereco[2].email 
+                )
+
             cy.get('.woocommerce-message').should('contain','Endereço alterado com sucesso.')
             
         });
